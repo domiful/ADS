07e6b6b932f31792f081c66af9c647b4d885aa68
domiful <draymond@riversagency.com>
2018-06-13 13:09:43 -0500

    First commit

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import EventsScreen from '../screens/EventsScreen'
+import SettingsScreen from '../screens/SettingsScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  Events: EventsScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Schedule',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-calendar${focused ? '' : '-outline'}`
+          : 'md-calendar'
+      }
+    />
+  ),
+};
+
+const LinksStack = createStackNavigator({
+  Links: LinksScreen,
+});
+
+LinksStack.navigationOptions = {
+  tabBarLabel: 'Chat',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-chatboxes${focused ? '' : '-outline'}` : 'md-chatboxes'}
+    />
+  ),
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Profile',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  LinksStack,
+  SettingsStack,
+});
---
+++ screens/EventsScreen.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ScrollView, StyleSheet, View, WebView } from 'react-native';
+import { ExpoLinksView } from '@expo/samples';
+
+export default class EventsScreen extends React.Component {
+  static navigationOptions = {
+    title: "Event",
+    headerStyle: {
+      backgroundColor: '#2864B3',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'normal',
+    },
+  };
+
+  render() {
+    let jsCode = `
+        alert(document.querySelector('.button-display.close').className);
+    `;
+   // let yourAlert = 'alert(document.getElementById("web-messenger-container").contentDocument.getElementById("mount").children.length)';
+    return (
+      <View style={styles.container}>
+        <WebView
+        source={{uri: "https://ads-gse00014266.uscom-east-1.oraclecloud.com/"}}
+        style={{ alignSelf: 'stretch', flex: 1,height: 900, marginTop:-50 }}
+        //injectedJavaScript={yourAlert}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
